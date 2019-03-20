5ba0cacf9a3b229e6ccf0447b28f7b4d21fb7572
mynesta <manuynesta@gmail.com>
2019-03-20 11:17:46 +0100

    apartat b, titol

--- app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.homersimpson_ynestamanuel;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-//
+
+    private ImageView titol;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titol = (ImageView) findViewById(R.id.imageTitul);
+        final AnimationDrawable anim = (AnimationDrawable) titol.getDrawable();
+        if(!anim.isRunning()){
+            anim.start();
+        }
+
+
     }
+
 }
