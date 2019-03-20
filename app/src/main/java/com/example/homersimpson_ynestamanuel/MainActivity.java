11efe2fd3a8e33af74c592a4f57247eaed66c4b2
mynesta <manuynesta@gmail.com>
2019-03-20 13:05:09 +0100

    apartat f y g

--- app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.homersimpson_ynestamanuel;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     Animation anim;
     Animation anim2;
     Animation anim3;
+    Animation anim4;
+    MediaPlayer media;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +48,12 @@ public class MainActivity extends AppCompatActivity {
             anim = AnimationUtils.loadAnimation(this, R.anim.animaciotween);
             anim2 = AnimationUtils.loadAnimation(this, R.anim.animaciotween2);
             anim3 = AnimationUtils.loadAnimation(this, R.anim.animaciotweendonut);
+            anim4 = AnimationUtils.loadAnimation(this, R.anim.animaciotweenull);
             engrVerd.startAnimation(anim2);
             engrVer.startAnimation(anim);
             engrBlau.startAnimation(anim);
             donut.startAnimation(anim3);
+            ull.startAnimation(anim4);
 
         }
         else{
@@ -55,12 +61,30 @@ public class MainActivity extends AppCompatActivity {
             engrVer.clearAnimation();
             engrVerd.clearAnimation();
             engrBlau.clearAnimation();
+            ull.clearAnimation();
             engrVer.setVisibility(View.INVISIBLE);
             engrVerd.setVisibility(View.INVISIBLE);
             engrBlau.setVisibility(View.INVISIBLE);
             ull.setVisibility(View.INVISIBLE);
             donut.setVisibility(View.INVISIBLE);
 
+        }
+
+    }
+
+    public void musica(View view) {
+        try
+        {
+            if (media != null && media.isPlaying())
+            {
+                media.stop();
+            }
+            else
+            {
+                media = MediaPlayer.create(this.getApplicationContext(), R.raw.the_simpsons);
+                media.start();
+            }
+        } catch(Exception e) {
         }
 
     }
