a8a8c45326da3dcc74ef5d3972578de0d9c0a463
mynesta <manuynesta@gmail.com>
2019-03-20 12:45:36 +0100

    apartat e

--- app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.homersimpson_ynestamanuel;
 
 import android.graphics.drawable.AnimationDrawable;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView titol,engrVer,engrVerd,engrBlau,ull,donut;
     Animation anim;
     Animation anim2;
+    Animation anim3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +43,15 @@ public class MainActivity extends AppCompatActivity {
             donut.setVisibility(View.VISIBLE);
             anim = AnimationUtils.loadAnimation(this, R.anim.animaciotween);
             anim2 = AnimationUtils.loadAnimation(this, R.anim.animaciotween2);
+            anim3 = AnimationUtils.loadAnimation(this, R.anim.animaciotweendonut);
             engrVerd.startAnimation(anim2);
             engrVer.startAnimation(anim);
             engrBlau.startAnimation(anim);
+            donut.startAnimation(anim3);
 
         }
         else{
+            donut.clearAnimation();
             engrVer.clearAnimation();
             engrVerd.clearAnimation();
             engrBlau.clearAnimation();
