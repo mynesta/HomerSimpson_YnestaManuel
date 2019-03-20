bd5d3187b99c91de3bd964e39e1a11db2fcfb9b5
mynesta <manuynesta@gmail.com>
2019-03-20 11:49:30 +0100

    apartat c

--- app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_ynestamanuel/MainActivity.java
@@ -9,13 +9,18 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView titol;
+    private ImageView titol,engrVer,engrVerd,engrBlau,ull,donut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         titol = (ImageView) findViewById(R.id.imageTitul);
+        engrVer = findViewById(R.id.imageEngVer);
+        engrVerd = findViewById(R.id.imageEngVerd);
+        engrBlau = findViewById(R.id.imageEngBlau);
+        ull = findViewById(R.id.imageUll);
+        donut = findViewById(R.id.imageDonut);
         final AnimationDrawable anim = (AnimationDrawable) titol.getDrawable();
         if(!anim.isRunning()){
             anim.start();
@@ -24,4 +29,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void titolClick(View view) {
+        if(engrVer.getVisibility() == View.INVISIBLE && engrVerd.getVisibility() == View.INVISIBLE &&
+                engrBlau.getVisibility() == View.INVISIBLE){
+            engrVer.setVisibility(View.VISIBLE);
+            engrVerd.setVisibility(View.VISIBLE);
+            engrBlau.setVisibility(View.VISIBLE);
+            ull.setVisibility(View.VISIBLE);
+            donut.setVisibility(View.VISIBLE);
+
+        }
+        else{
+            engrVer.setVisibility(View.INVISIBLE);
+            engrVerd.setVisibility(View.INVISIBLE);
+            engrBlau.setVisibility(View.INVISIBLE);
+            ull.setVisibility(View.INVISIBLE);
+            donut.setVisibility(View.INVISIBLE);
+        }
+
+    }
 }
